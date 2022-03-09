0ab7f924fdc4f74102233882294ec435d608d926
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-03-09 18:23:19 -0300

    feat: adding and positioning images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,42 @@
 import React from "react"
 import Layout from "../components/Layout"
 import "../styles/global.css"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Parallax Scroll Testing</h1>
+      <section>
+        <StaticImage
+          src="../assets/images/stars.png"
+          alt="stars"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+          className="images"
+        />
+        <StaticImage
+          src="../assets/images/moon.png"
+          alt="the moon"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+          className="images"
+          id="moon"
+        />
+        <StaticImage
+          src="../assets/images/mountains_behind.png"
+          alt="behind mountains"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+          className="images"
+        />
+        <StaticImage
+          src="../assets/images/mountains_front.png"
+          alt="front mountains"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+          className="images"
+        />
+      </section>
     </Layout>
   )
 }
